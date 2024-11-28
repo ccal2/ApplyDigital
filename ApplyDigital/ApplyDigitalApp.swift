c5fddb9b6b426ab17d4057ad8bb1ba4b504a6d9d
Carolina Lopes <ccruzagralopes@apple.com>
2024-11-28 13:07:40 -0300

    Initial Commit

---
+++ ApplyDigital/ApplyDigitalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplyDigitalApp.swift
+//  ApplyDigital
+//
+//  Created by Carolina Lopes on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ApplyDigitalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
